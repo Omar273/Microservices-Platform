53732e1785b868d3ea22180096d43abe6bdcc851
Mohamed Omar BACHA <62521684+Omar273@users.noreply.github.com>
2026-03-05 14:57:57 +0100

    Update DATABASE_URL to use secrets.DB_U

--- task-service/app/database.py
+++ task-service/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@task-db:5432/taskdb")
+DATABASE_URL = os.getenv("DATABASE_URL", secrets.DB_U)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
